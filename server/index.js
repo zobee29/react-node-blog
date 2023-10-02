88ea391be3b4119c806e947a12181668cbe1d4f6
Zohaib Farooqi <zohaib.farooqi029@gmail.com>
2023-10-02 10:36:41 -0500

    Remove CORS for now

--- server/index.js
+++ server/index.js
@@ -1,11 +1,8 @@
 const express = require("express");
-const cors = require("cors");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-
-app.use(cors());
 
 app.get("/api", (req, res) => {
   console.log("/api")
