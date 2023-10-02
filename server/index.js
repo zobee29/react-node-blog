b4c83b1abf7559d6d4fc3771e4270684b10e3b25
Zohaib Farooqi <zohaib.farooqi029@gmail.com>
2023-10-02 10:27:03 -0500

    First Commit: Setting up Node, Express and React

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(cors());
+
+app.get("/api", (req, res) => {
+  console.log("/api")
+  res.json({ message: "Hello from server!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
